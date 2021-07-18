649c7ff9b0ee0021880ad4dd88be272731533a74
RDzRyan <axiooryan@gmail.com>
2021-07-18 15:05:31 +0700

    clean up

--- hexapod_teleop_joystick/src/hexapod_teleop_joystick.cpp
+++ hexapod_teleop_joystick/src/hexapod_teleop_joystick.cpp
@@ -35,7 +35,7 @@
 
 HexapodTeleopJoystick::HexapodTeleopJoystick(void)
 {
-    state_.data = true;
+    state_.data = false;
     imu_override_.data = false;
     NON_TELEOP = false; // Static but here for a safety precaution
     ros::param::get("STANDUP_BUTTON", STANDUP_BUTTON);
