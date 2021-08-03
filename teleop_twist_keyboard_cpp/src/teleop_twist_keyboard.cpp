7e3b0a74d792d7cc82bcc880c46659339f9dcbda
RDzRyan <axiooryan@gmail.com>
2021-08-03 15:13:22 +0700

    Update

--- teleop_twist_keyboard_cpp/src/teleop_twist_keyboard.cpp
+++ teleop_twist_keyboard_cpp/src/teleop_twist_keyboard.cpp
@@ -73,8 +73,8 @@ CTRL-C to quit
 )";
 
 // Init variables
-float speed(0.15); // Linear velocity (m/s)
-float turn(0.8); // Angular velocity (rad/s)
+float speed(0.5); // Linear velocity (m/s)
+float turn(1.0); // Angular velocity (rad/s)
 float x(0), y(0), z(0), th(0); // Forward/backward/neutral direction vars
 char key(' ');
 
