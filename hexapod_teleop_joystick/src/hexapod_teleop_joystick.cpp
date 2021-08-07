f13a9f027279c0407bf451014927168e032b4f7d
RDzRyan <axiooryan@gmail.com>
2021-08-07 13:56:56 +0700

    update

--- hexapod_teleop_joystick/src/hexapod_teleop_joystick.cpp
+++ hexapod_teleop_joystick/src/hexapod_teleop_joystick.cpp
@@ -29,6 +29,36 @@
 
 #include <hexapod_teleop_joystick.h>
 
+/////////////////////////////////////////////////////////
+int getch(void)
+{
+    int ch;
+    struct termios oldt;
+    struct termios newt;
+
+    // Store old settings, and copy to new settings
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+
+    // Make required changes and apply the settings
+    newt.c_lflag &= ~(ICANON | ECHO);
+    newt.c_iflag |= IGNBRK;
+    newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
+    newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
+    newt.c_cc[VMIN] = 1;
+    newt.c_cc[VTIME] = 0;
+    tcsetattr(fileno(stdin), TCSANOW, &newt);
+
+    // Get the current character
+    ch = getchar();
+
+    // Reapply old settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    return ch;
+}
+////////////////////////////////////////////////////////////
+
 //==============================================================================
 // Constructor
 //==============================================================================
@@ -114,10 +144,6 @@ HexapodTeleopJoystick::HexapodTeleopJoystick(void)
     // Update the Twist message
     cmd_vel_pub_.linear.x = x * speed;
     cmd_vel_pub_.linear.y = y * speed;
-    cmd_vel_pub_.linear.z = z * speed;
-
-    cmd_vel_pub_.angular.x = 0;
-    cmd_vel_pub_.angular.y = 0;
     cmd_vel_pub_.angular.z = th * turn;
 }
 
@@ -168,36 +194,6 @@ void HexapodTeleopJoystick::joyCallback(const sensor_msgs::Joy::ConstPtr &joy)
         cmd_vel_.angular.z = joy->axes[YAW_ROTATION_AXES] * MAX_RADIANS_PER_SEC;
     }
 }
-
-/////////////////////////////////////////////////////////
-int getch(void)
-{
-    int ch;
-    struct termios oldt;
-    struct termios newt;
-
-    // Store old settings, and copy to new settings
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-
-    // Make required changes and apply the settings
-    newt.c_lflag &= ~(ICANON | ECHO);
-    newt.c_iflag |= IGNBRK;
-    newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
-    newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
-    newt.c_cc[VMIN] = 1;
-    newt.c_cc[VTIME] = 0;
-    tcsetattr(fileno(stdin), TCSANOW, &newt);
-
-    // Get the current character
-    ch = getchar();
-
-    // Reapply old settings
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-    return ch;
-}
-////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv)
 {
