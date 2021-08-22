fc106e47951ece7826122f6b0889e94b8d2d2793
aditia08 <dit1a241@gmail.com>
2021-08-22 19:13:24 +0700

    a

--- hexapod_controller/include/control.h
+++ hexapod_controller/include/control.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <std_srvs/Empty.h>
 #include <std_msgs/Bool.h>
 #include <geometry_msgs/Vector3.h>
