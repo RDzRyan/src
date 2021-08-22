3e47b5632d34ca6c770c6af547318ee54afde6d4
aditia08 <dit1a241@gmail.com>
2021-08-22 21:23:31 +0700

    a

--- hexapod_controller/src/control.cpp
+++ hexapod_controller/src/control.cpp
@@ -77,7 +77,10 @@ Control::Control(void)
 
 
 void Control::set_initial_2d(const geometry_msgs::PoseStamped &rvizClick){
-
+    odomOld.pose.pose.position.x = rvizClick.pose.position.x;
+    odomOld.pose.pose.position.y = rvizClick.pose.position.y;
+    odomOld.pose.pose.orientation.z = rvizClick.pose.orientation.z;
+    initialPoseRecieved = true;
 }
 
 
