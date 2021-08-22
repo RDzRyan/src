e87066c6b9e523f771121ff3ecd5c52d7bfe5e2a
aditia08 <dit1a241@gmail.com>
2021-08-22 20:56:48 +0700

    a

--- hexapod_controller/src/hexapod_controller.cpp
+++ hexapod_controller/src/hexapod_controller.cpp
@@ -13,6 +13,19 @@ const double initialTheta = 0.00000000001;
 const double PI = 3.141592;
 // Flag to see if initial pose has been received
 bool initialPoseRecieved = false;
+void set_initial_2d(const geometry_msgs::PoseStamped &rvizClick) {
+ 
+  odomOld.pose.pose.position.x = rvizClick.pose.position.x;
+  odomOld.pose.pose.position.y = rvizClick.pose.position.y;
+  odomOld.pose.pose.orientation.z = rvizClick.pose.orientation.z;
+  initialPoseRecieved = true;
+}void set_initial_2d(const geometry_msgs::PoseStamped &rvizClick) {
+ 
+  odomOld.pose.pose.position.x = rvizClick.pose.position.x;
+  odomOld.pose.pose.position.y = rvizClick.pose.position.y;
+  odomOld.pose.pose.orientation.z = rvizClick.pose.orientation.z;
+  initialPoseRecieved = true;
+}
  
 //=============================================================================
 // Main
