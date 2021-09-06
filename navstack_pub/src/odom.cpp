266e9e9c654d39c5bcdb1a4b7c75982490af0b68
aditia08 <dit1a241@gmail.com>
2021-09-07 06:18:27 +0700

    a

--- navstack_pub/src/odom.cpp
+++ navstack_pub/src/odom.cpp
@@ -2,18 +2,17 @@
 #include "std_msgs/String.h"
 #include <nav_msgs/Odometry.h>
 //odom_pub_ = nh_.advertise<nav_msgs::Odometry>("/odom_data_quat", 50);
-ros::Time waktu;
-nav_msgs::Odometry gerak_;
+ros::Time waktu;;
 
 void chatterCallback(const nav_msgs::Odometryr& odom)
 {
-  waktu =ros::Time::now();
+  gerak_.header.stamp=odom.header.stamp;
   gerak_.pose.pose.position.x=odom.pose.pose.position.x;
   gerak_.pose.pose.position=odom.pose.pose.position.y;
   gerak_.pose.pose.position=odom.pose.pose.position.z;
   gerak_.pose.pose.orientation.z=odom.pose.pose.orientation.z;
   gerak_.pose.pose.orientation.w=odom.pose.pose.orientation.w;
-  ROS_INFO("%s, %f, %f, %f, %f, %f,", waktu,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
+  ROS_INFO("%d, %f, %f, %f, %f, %f,", gerak_.header.stamp,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
 }
  
 int main(int argc, char **argv)
