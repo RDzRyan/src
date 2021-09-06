ad636d5404a4205e44d114f2b7ba39b4d81f6a3f
aditia08 <dit1a241@gmail.com>
2021-09-07 06:16:02 +0700

    a

--- navstack_pub/src/odom.cpp
+++ navstack_pub/src/odom.cpp
@@ -13,7 +13,7 @@ void chatterCallback(const nav_msgs::Odometryr& odom)
   gerak_.pose.pose.position=odom.pose.pose.position.z;
   gerak_.pose.pose.orientation.z=odom.pose.pose.orientation.z;
   gerak_.pose.pose.orientation.w=odom.pose.pose.orientation.w;
-  ROS_INFO("%f, %f, %f, %f, %f, %f,", waktu,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
+  ROS_INFO("%s, %f, %f, %f, %f, %f,", waktu,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
 }
  
 int main(int argc, char **argv)
