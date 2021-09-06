ecca71b5b26f15d3d92e4cf0e35944439fa077fa
aditia08 <dit1a241@gmail.com>
2021-09-07 06:40:01 +0700

    a

--- navstack_pub/src/odom_read.cpp
+++ navstack_pub/src/odom_read.cpp
@@ -3,17 +3,18 @@
 #include <nav_msgs/Odometry.h>
 //odom_pub_ = nh_.advertise<nav_msgs::Odometry>("/odom_data_quat", 50);
 nav_msgs::Odometry gerak_;
+double secs;
 
 void chatterCallback(const nav_msgs::Odometry& odom)
 {
   // gerak_.header.stamp=odom.header.stamp;
-  double secs =ros::Time::now().toSec();
+  secs =ros::Time::now().toSec();
   gerak_.pose.pose.position.x=odom.pose.pose.position.x;
   gerak_.pose.pose.position.y=odom.pose.pose.position.y;
   gerak_.pose.pose.position.z=odom.pose.pose.position.z;
   gerak_.pose.pose.orientation.z=odom.pose.pose.orientation.z;
   gerak_.pose.pose.orientation.w=odom.pose.pose.orientation.w;
-  ROS_INFO("%f, %f, %f, %f, %f, %f,", secs,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
+  
 }
  
 int main(int argc, char **argv)
@@ -21,8 +22,14 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "read_odom");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("odom_data_quat", 50, chatterCallback);
-  ros::spin();
-  ros::Duration(0.5).sleep();
- 
+  
+  ros::Rate r(10); // 10 hz
+  while (ros::ok())
+  {
+    //... do some work ...
+    ROS_INFO("%f, %f, %f, %f, %f, %f,", secs,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
+    ros::spin();
+    r.sleep();
+  }
   return 0;
 }
