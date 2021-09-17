3a259505629d29943d92fe4b8c625b5f880309ae
aditia08 <dit1a241@gmail.com>
2021-09-17 14:05:00 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -1,33 +1,22 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <nav_msgs/Odometry.h>
-//odom_pub_ = nh_.advertise<nav_msgs::Odometry>("/odom_data_quat", 50);
-nav_msgs::Odometry gerak_;
-double secs;
 
 void chatterCallback(const nav_msgs::Odometry& odom)
 {
-  // gerak_.header.stamp=odom.header.stamp;
-  secs =ros::Time::now().toSec();
-  gerak_.pose.pose.position.x=odom.pose.pose.position.x;
-  gerak_.pose.pose.position.y=odom.pose.pose.position.y;
-  gerak_.pose.pose.position.z=odom.pose.pose.position.z;
-  gerak_.pose.pose.orientation.z=odom.pose.pose.orientation.z;
-  gerak_.pose.pose.orientation.w=odom.pose.pose.orientation.w;
-  
+ 
 }
  
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "read_odom");
+  ros::init(argc, argv, "Move_Control");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("odom_data_quat", 50, chatterCallback);
+  // ros::Subscriber sub = n.subscribe("odom_data_quat", 50, chatterCallback);
   
-  ros::Rate r(1); 
+  ros::Rate r(200); 
   while (ros::ok())
   {
     //... do some work ...
-    ROS_INFO("%f, %f, %f, %f, %f, %f,", secs,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
+    // ROS_INFO("%f, %f, %f, %f, %f, %f,", secs,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
     ros::spinOnce();
     r.sleep();
   }
