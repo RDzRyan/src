ab0ca92d855db328a907746d80f2057b65aec58c
aditia08 <dit1a241@gmail.com>
2021-09-17 14:38:46 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -2,6 +2,8 @@
 #include "std_msgs/String.h"
 #include "sensor_msgs/LaserScan.h"
 
+#include <nav_msgs/Odometry.h>
+
 #define RAD2DEG(x) ((x)*180./M_PI)
 float laser[9];
 
@@ -15,18 +17,43 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
     for(int i = 0; i < 9; i++) {
       laser[i] =scan->ranges[i*80]);
       float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i*80);
-      ROS_INFO(": [%f, %f]", degree, laser[i]);
+      // ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
     }
 
+}
+
+nav_msgs::Odometry gerak_;
+double secs;
+
+void chatterCallback(const nav_msgs::Odometry& odom)
+{
+  // gerak_.header.stamp=odom.header.stamp;
+  secs =ros::Time::now().toSec();
+  gerak_.pose.pose.position.x=odom.pose.pose.position.x;
+  gerak_.pose.pose.position.y=odom.pose.pose.position.y;
+  gerak_.pose.pose.position.z=odom.pose.pose.position.z;
+  gerak_.pose.pose.orientation.z=odom.pose.pose.orientation.z;
+  gerak_.pose.pose.orientation.w=odom.pose.pose.orientation.w;
+  
 }
  
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "Move_Control");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 50, scanCallback);
+  ros::Subscriber sub = n.subscribe("/scan", 50, scanCallback);
+  ros::Subscriber sub = n.subscribe("odom_data_quat", 50, chatterCallback);
 
-    ros::spin();
-
-    return 0;
+  ros::Rate r(1); 
+  while (ros::ok())
+  {
+    //... do some work ...
+    ROS_INFO("%f, %f, %f, %f, %f, %f,", secs,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
+    for(int i = 0; i < 9; i++) {
+      ROS_INFO(": [%f]", laser[i]);
+    }
+    ros::spinOnce();
+    r.sleep();
+  }
+  return 0;
 }
