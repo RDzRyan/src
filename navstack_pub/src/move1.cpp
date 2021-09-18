93179e271af18a3dec1d52224866cd4514ee6161
aditia08 <dit1a241@gmail.com>
2021-09-18 15:53:25 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -131,14 +131,15 @@ int main(int argc, char **argv)
   ros::Rate r(100); 
   while (ros::ok())
   {
-    // ... do some work ...
-    ROS_INFO("%f, %f, %f, %f, %f, %f,", secs,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
-    for(int i = 0; i < 9; i++) {
-      ROS_INFO(": [%f]", laser[i]);
-    }
-    
-      // kontrol(a_gerak[flag1],batasan0[flag1],batasan1[flag1],batasan2[flag1],batasan3[flag1],batasan4[flag1],batasan5[flag1],batasan6[flag1],batasan7[flag1],batasan8[flag1]);
-      // ROS_INFO("step: %d", flag1);
+    //baca setpoin
+    // ROS_INFO("%f, %f, %f, %f, %f, %f,", secs,gerak_.pose.pose.position.x,gerak_.pose.pose.position.y,gerak_.pose.pose.position.z,gerak_.pose.pose.orientation.z,gerak_.pose.pose.orientation.w);
+    // for(int i = 0; i < 9; i++) {
+    //   ROS_INFO(": [%f]", laser[i]);
+    // }
+
+    //eksekusu
+      kontrol(a_gerak[flag1],batasan0[flag1],batasan1[flag1],batasan2[flag1],batasan3[flag1],batasan4[flag1],batasan5[flag1],batasan6[flag1],batasan7[flag1],batasan8[flag1]);
+      ROS_INFO("step: %d", flag1);
 
 
     ros::spinOnce();
