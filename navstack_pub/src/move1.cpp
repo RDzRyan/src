157e03f64b5a0e43b9f3bbbe1e47ba72287054cc
aditia08 <dit1a241@gmail.com>
2021-09-18 13:19:34 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -122,7 +122,7 @@ int main(int argc, char **argv)
 
   //ros::Publisher pub = n.advertise("cmd_vel", 1); //<geometry_msgs::Twist>
 
-  ros::Rate r(100); 
+  ros::Rate r(1); 
   while (ros::ok())
   {
     //... do some work ...
