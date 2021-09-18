094b12aeac3ee9c09ae1ad97adff3b1a60d8faa0
aditia08 <dit1a241@gmail.com>
2021-09-18 10:35:20 +0700

    teleop_move

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -4,6 +4,15 @@
 
 #include <nav_msgs/Odometry.h>
 
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/AccelStamped.h>
+#include <std_msgs/Bool.h>
+#include <sensor_msgs/Imu.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <map>
+
 #define RAD2DEG(x) ((x)*180./M_PI)
 float laser[9];
 
@@ -36,6 +45,35 @@ void chatterCallback(const nav_msgs::Odometry& odom)
   gerak_.pose.pose.orientation.w=odom.pose.pose.orientation.w;
   
 }
+
+// Map for movement keys
+std::map<char, std::vector<float>> moveBindings{
+    //Moving and Rotating
+    {'q', {1, 0, 0, 1}},
+    {'w', {1, 0, 0, 0}},
+    {'e', {1, 0, 0, -1}},
+    {'a', {0, 0, 0, 1}},
+    {'s', {0, 0, 0, 0}},
+    {'d', {0, 0, 0, -1}},
+    {'z', {-1, 0, 0, -1}},
+    {'x', {-1, 0, 0, 0}},
+    {'c', {-1, 0, 0, 1}},
+    //Holomonic Move
+    {'Q', {1, -1, 0, 0}},
+    {'W', {1, 0, 0, 0}},
+    {'E', {1, 1, 0, 0}},
+    {'A', {0, -1, 0, 0}},
+    {'S', {0, 0, 0, 0}},
+    {'D', {0, 1, 0, 0}},
+    {'Z', {-1, -1, 0, 0}},
+    {'X', {-1, 0, 0, 0}},
+    {'C', {-1, 1, 0, 0}}};
+
+// Init variables
+float speed(1.0);                                                 // Linear velocity (m/s)
+float turn(1.0);                                                  // Angular velocity (rad/s)
+float x(0), y(0), z(0), xa(0), ya(0), za(0), xb(0), yb(0), th(0); // Forward/backward/neutral direction vars
+char key(' ');
 
 void kontrol(string arah_, float batas0,float batas1,float batas2,float batas3,float batas4,float batas5,float batas6,float batas7,float batas8,nav_msgs::Odometry posisi_){
   // cek batas
