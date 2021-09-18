ef7f628715363052cfa35fe38b9f4bf54ccfc664
aditia08 <dit1a241@gmail.com>
2021-09-18 16:14:56 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -126,7 +126,7 @@ int main(int argc, char **argv)
   ros::Subscriber sub = n.subscribe("/scan", 50, scanCallback);
   ros::Subscriber sub1 = n.subscribe("/odom_data_quat", 50, chatterCallback);
 
-  ros::Publisher pub = n.advertise("cmd_vel", 1); //<geometry_msgs::Twist>
+  ros::Publisher pub = n.advertise("/cmd_vel", 1); //<geometry_msgs::Twist>
   flag1=0;
   ros::Rate r(100); 
   while (ros::ok())
