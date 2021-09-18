9a03cf8952f72f9532e81dcbaf28b639e8b8cf43
aditia08 <dit1a241@gmail.com>
2021-09-18 11:41:58 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -19,7 +19,7 @@ float laser[9];
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
     int count = scan->scan_time / scan->time_increment;
-    ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
+    // ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
     // ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
     // ROS_INFO("aaaa %d, %d",scan->scan_time,scan->time_increment);
   
