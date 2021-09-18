0e789b3ed90468f7d13f1c02ecfc13e4504769d6
aditia08 <dit1a241@gmail.com>
2021-09-18 16:03:03 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -20,9 +20,9 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
     for(int i = 0; i < 9; i++) {
       laser[i] =scan->ranges[i*80];
-      if (scan->ranges[i*80]== ynf){
-        laser[i]=3;
-      }
+      // if (scan->ranges[i*80]== ynf){
+      //   laser[i]=3;
+      // }
       float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i*80);
       // ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
     }
