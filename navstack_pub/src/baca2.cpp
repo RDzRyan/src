5c6840bbe45616d20ec1d52eeacaadaf3959e220
aditia08 <dit1a241@gmail.com>
2021-09-21 00:27:15 +0700

    a

--- navstack_pub/src/baca2.cpp
+++ navstack_pub/src/baca2.cpp
@@ -20,11 +20,8 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
     for(int i = 0; i < 8; i++) {
       laser[i] =scan->ranges[i*90];
-      // if (scan->ranges[i*80]== ynf){
-      //   laser[i]=3;
-      // }
+      
       float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i*90);
-      // ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
     }
 
 }
@@ -68,17 +65,7 @@ std::map<char, std::vector<float>> moveBindings{
     {'X', {-1, 0, 0, 0}},
     {'C', {-1, 1, 0, 0}}};
 
-//step
-char a_gerak[]  ={'D'   ,'D'  ,'s'};
-float batasan0[]={6     ,6    };
-float batasan1[]={6     ,0.385};
-float batasan2[]={6     ,6    };
-float batasan3[]={0.44  ,0.51 };
-float batasan4[]={0.233 ,0.51 };
-float batasan5[]={0.232 ,0.685};
-float batasan6[]={6     ,6    };
-float batasan7[]={6     ,6    };
-float batasan8[]={6     ,6    };
+
 
 // Init variables
 float speed(0.5);                                                 // Linear velocity (m/s)
@@ -88,38 +75,7 @@ char key(' ');
 geometry_msgs::Twist twist;
 int flag1=0;
 // void kontrol(char arah_, float batas[9]){ //,nav_msgs::Odometry posisi_
-void kontrol(char arah_, float batas0,float batas1,float batas2,float batas3,float batas4,float batas5,float batas6,float batas7,float batas8){
-  
-    key=arah_;
-  if (moveBindings.count(key) == 1)
-    {
-      // Grab the direction data
-      x = moveBindings[key][0];
-      y = moveBindings[key][1];
-      z = moveBindings[key][2];
-      th = moveBindings[key][3];
-      // imu_override_.data = false;
-      ROS_INFO("\rCurrent: speed %f\tturn %f | Last command: %c   ", speed, turn, key);
-      ROS_INFO("%f, %f, %f, %f, %f, %f,%f, %f, %f,",batas0, batas1, batas2,batas3, batas4, batas5,batas6, batas7, batas8);
-       ROS_INFO("%f, %f, %f, %f, %f, %f,%f, %f, %f,",laser[0],laser[1],laser[2],laser[3],laser[4],laser[5],laser[6],laser[7],laser[8]);
-    }
 
-     // Update the Twist message
-    twist.linear.x = x * speed;
-    twist.linear.y = y * speed;
-    twist.linear.z = z * speed;
-
-    twist.angular.x = 0;
-    twist.angular.y = 0;
-    twist.angular.z = th * turn;
-    
-    
-    if (laser[0]<=batas0 && laser[1]<=batas1 && laser[2]<=batas2 && laser[3]<=batas3 && laser[4]<=batas4 && laser[5]<=batas5 && laser[6]<=batas6 && laser[7]<=batas7 && laser[8]<=batas8 ){
-      flag1++;
-      ROS_INFO("clear");
-    }
-  
-}
 
 // int mode;
 // void pergerakan(int mode_){ 
@@ -150,12 +106,7 @@ int main(int argc, char **argv)
       ROS_INFO(": [%f]", laser[i]);
     }
 
-    //eksekusi
-      // kontrol(a_gerak[flag1],batasan0[flag1],batasan1[flag1],batasan2[flag1],batasan3[flag1],batasan4[flag1],batasan5[flag1],batasan6[flag1],batasan7[flag1],batasan8[flag1]);
-      // pub.publish(twist);
-      // ROS_INFO("step: %d", flag1);
-
-
+    
     ros::spinOnce();
     r.sleep();
   }
