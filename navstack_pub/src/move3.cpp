009fbadd20a1b14fc4886e2b848708184d7dd4e2
aditia08 <dit1a241@gmail.com>
2021-09-21 00:14:46 +0700

    a

--- navstack_pub/src/move3.cpp
+++ navstack_pub/src/move3.cpp
@@ -147,6 +147,7 @@ void kontrol(char arah_, int step_){
         else{s[a]=false;}
       }
     }
+  ROS_INFO("%d, %d, %d, %d, %d, %d, %d, %d, ",s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7]);
 }
 
 
