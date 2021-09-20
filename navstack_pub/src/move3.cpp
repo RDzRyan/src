59e78bd5fd0a0eaebd4eee5340ea40fd8c5e448e
aditia08 <dit1a241@gmail.com>
2021-09-20 23:31:01 +0700

    a

--- navstack_pub/src/move3.cpp
+++ navstack_pub/src/move3.cpp
@@ -20,12 +20,8 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
     for(int i = 0; i < 8; i++) {
       laser[i] =scan->ranges[i*90];
-      // if (scan->ranges[i*80]== ynf){
-      //   laser[i]=3;
-      // }
       float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i*90);
-      // ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
-    }
+      }
 
 }
 
@@ -69,14 +65,6 @@ std::map<char, std::vector<float>> moveBindings{
 
 //step
 char a_gerak[]  ={'D','s'};
-// std::map<int, std::vector<float>> step{
-//     //Moving and Rotating
-//     {0, {6, 6, 6, 6, 6, 6, 6, 6, 6 }},
-//     {1, {6, 6, 6, 6, 6, 6, 6, 6,6}}};
-// std::map<int,std::vector<bool>> _flag_{
-//     //Moving and Rotating
-//     {0, {true,true,true,true,true,true,true,true}},
-//     {1, {true,true,true,true,true,true,true,true}}};
 float step1[][]={
   {6, 6, 6, 6, 6, 6, 6, 6, 6},
   {6, 6, 6, 6, 6, 6, 6, 6, 6}
@@ -93,68 +81,57 @@ float x(0), y(0), z(0), xa(0), ya(0), za(0), xb(0), yb(0), th(0); // Forward/bac
 char key(' ');
 geometry_msgs::Twist twist;
 int flag1=0;
-// void kontrol(char arah_, float batas[9]){ //,nav_msgs::Odometry posisi_
-// void kontrol(char arah_, float batas[0], float batas[1], float batas[2], float batas[3], float batas[4], float batas[5], float batas[6], float batas[7],
-//   bool _f[0], bool _f[1], bool _f[2], bool _f[3], bool _f[4], bool _f[5], bool _f[6], bool _f[7],){
-  void kontrol(char arah_, float batas[][],bool _f[][]){
-    key=arah_;
-  if (moveBindings.count(key) == 1)
-    {
-      // Grab the direction data
-      x = moveBindings[key][0];
-      y = moveBindings[key][1];
-      z = moveBindings[key][2];
-      th = moveBindings[key][3];
-      // imu_override_.data = false;
-      ROS_INFO("\rCurrent: speed %f\tturn %f | Last command: %c   ", speed, turn, key);
-      }
-      ROS_INFO("%f, %f, %f, %f, %f, %f,%f, %f,", batas[0], batas[1], batas[2], batas[3], batas[4], batas[5], batas[6], batas[7]);
-      ROS_INFO("%f, %f, %f, %f, %f, %f,%f, %f, %f,",laser[0],laser[1],laser[2],laser[3],laser[4],laser[5],laser[6],laser[7],laser[8]);
 
-     // Update the Twist message
-    twist.linear.x = x * speed;
-    twist.linear.y = y * speed;
-    twist.linear.z = z * speed;
+// void kontrol(char arah_, float batas[][],bool _f[][]){
+//     key=arah_;
+//   if (moveBindings.count(key) == 1)
+//     {
+//       // Grab the direction data
+//       x = moveBindings[key][0];
+//       y = moveBindings[key][1];
+//       z = moveBindings[key][2];
+//       th = moveBindings[key][3];
+//       // imu_override_.data = false;
+//       ROS_INFO("\rCurrent: speed %f\tturn %f | Last command: %c   ", speed, turn, key);
+//       }
+//       ROS_INFO("%f, %f, %f, %f, %f, %f,%f, %f,", batas[0], batas[1], batas[2], batas[3], batas[4], batas[5], batas[6], batas[7]);
+//       ROS_INFO("%f, %f, %f, %f, %f, %f,%f, %f, %f,",laser[0],laser[1],laser[2],laser[3],laser[4],laser[5],laser[6],laser[7],laser[8]);
 
-    twist.angular.x = 0;
-    twist.angular.y = 0;
-    twist.angular.z = th * turn;
+//      // Update the Twist message
+//     twist.linear.x = x * speed;
+//     twist.linear.y = y * speed;
+//     twist.linear.z = z * speed;
+
+//     twist.angular.x = 0;
+//     twist.angular.y = 0;
+//     twist.angular.z = th * turn;
     
-    bool s[8]={false,false,false,false,false,false,false,false};
+//     bool s[8]={false,false,false,false,false,false,false,false};
 
-    for (int a=0; a<8; a++){
-      if(_f[a]==true){
-        if(laser[a]<=batas[a])
-        {
-          s[a]=true;
-        }
-        else{s[a]=false;}
-      }
-      else{
-        if(laser[a]>=batas[a])
-        {
-          s[a]=true;
-        }
-        else{s[a]=false;}
-      }
-    }
+//     for (int a=0; a<8; a++){
+//       if(_f[a]==true){
+//         if(laser[a]<=batas[a])
+//         {
+//           s[a]=true;
+//         }
+//         else{s[a]=false;}
+//       }
+//       else{
+//         if(laser[a]>=batas[a])
+//         {
+//           s[a]=true;
+//         }
+//         else{s[a]=false;}
+//       }
+//     }
 
-    if (batas[0]==true && batas[1]==true && batas[2]==true && batas[3]==true && batas[4]==true && batas[5]==true && batas[6]==true && batas[7]==true){
-      flag1++;
-      ROS_INFO("clear");
-    }
+//     if (batas[0]==true && batas[1]==true && batas[2]==true && batas[3]==true && batas[4]==true && batas[5]==true && batas[6]==true && batas[7]==true){
+//       flag1++;
+//       ROS_INFO("clear");
+//     }
   
-}
-
-// int mode;
-// void pergerakan(int mode_){ 
-//   while(mode_==1){
-
-//   }
-//   while(mode_==2){
-
-//   }
 // }
+
  
 int main(int argc, char **argv)
 {
@@ -164,7 +141,6 @@ int main(int argc, char **argv)
   ros::Subscriber sub1 = n.subscribe("/odom_data_quat", 50, chatterCallback);
 
   ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1); 
-  // ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
   flag1=0;
   ros::Rate r(30); 
   while (ros::ok())
@@ -176,7 +152,6 @@ int main(int argc, char **argv)
     // }
 
     //eksekusi
-      // kontrol(a_gerak[flag1],step[flag1][0], step[flag1][1], step[flag1][2], step[flag1][3],step[flag1][4],step[flag1][5],step[flag1][6],step[flag1][7],step[flag1][8],_flag_[flag1][0], _flag_[flag1][1], _flag_[flag1][2], _flag_[flag1][3], _flag_[flag1][4], _flag_[flag1][5], _flag_[flag1][6], _flag_[flag1][7]);
       kontrol(a_gerak[flag1],step[flag1][],_flag_[flag1][]);
       pub.publish(twist);
       ROS_INFO("step: %d", flag1);
