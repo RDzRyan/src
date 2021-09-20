84ea5f007dc483c4a73d85c09863870e2673c598
aditia08 <dit1a241@gmail.com>
2021-09-20 23:13:50 +0700

    a

--- navstack_pub/src/move3.cpp
+++ navstack_pub/src/move3.cpp
@@ -71,8 +71,8 @@ std::map<char, std::vector<float>> moveBindings{
 char a_gerak[]  ={'D','s'};
 std::map<int, std::vector<float>> step{
     //Moving and Rotating
-    {0, {6, 6, 6, 6, 6, 6, 6, 6,6 }},
-    {1, {6, 6, 6, 6, 6, 6, 6, 6,6 }}};
+    {0, {6, 6, 6, 6, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1, 1, 1 }},
+    {1, {6, 6, 6, 6, 6, 6, 6, 6,6, 1, 1, 1, 1, 1, 1, 1, 1}}};
 std::map<int,std::vector<bool>> _flag_{
     //Moving and Rotating
     {0, {true,true,true,true,true,true,true,true,}},
@@ -89,7 +89,7 @@ int flag1=0;
 // void kontrol(char arah_, float batas[9]){ //,nav_msgs::Odometry posisi_
 // void kontrol(char arah_, float batas[0], float batas[1], float batas[2], float batas[3], float batas[4], float batas[5], float batas[6], float batas[7],
 //   bool _f[0], bool _f[1], bool _f[2], bool _f[3], bool _f[4], bool _f[5], bool _f[6], bool _f[7],){
-  void kontrol(char arah_, float* batas, bool* _f){
+  void kontrol(char arah_, float* batas,bool* _f){
     key=arah_;
   if (moveBindings.count(key) == 1)
     {
@@ -169,7 +169,8 @@ int main(int argc, char **argv)
     // }
 
     //eksekusi
-      kontrol(a_gerak[flag1],step[flag1][0], step[flag1][1], step[flag1][2], step[flag1][3],step[flag1][4],step[flag1][5],step[flag1][6],step[flag1][7],step[flag1][8],_flag_[flag1][0], _flag_[flag1][1], _flag_[flag1][2], _flag_[flag1][3], _flag_[flag1][4], _flag_[flag1][5], _flag_[flag1][6], _flag_[flag1][7]);
+      // kontrol(a_gerak[flag1],step[flag1][0], step[flag1][1], step[flag1][2], step[flag1][3],step[flag1][4],step[flag1][5],step[flag1][6],step[flag1][7],step[flag1][8],_flag_[flag1][0], _flag_[flag1][1], _flag_[flag1][2], _flag_[flag1][3], _flag_[flag1][4], _flag_[flag1][5], _flag_[flag1][6], _flag_[flag1][7]);
+      kontrol(a_gerak[flag1],step[flag1][],_flag_[flag1][]);
       pub.publish(twist);
       ROS_INFO("step: %d", flag1);
 
