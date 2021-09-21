752ba573c620064bf89bea8facc17dd1cf4a92d6
aditia08 <dit1a241@gmail.com>
2021-09-21 10:36:35 +0700

    a

--- rplidar_ros/src/node.cpp
+++ rplidar_ros/src/node.cpp
@@ -76,8 +76,8 @@ void publish_scan(ros::Publisher *pub,
     scan_msg.scan_time = scan_time;
     scan_msg.time_increment = scan_time / (double)(node_count-1);
     scan_msg.range_min = 0.15;
-    // scan_msg.range_max = max_distance;//8.0;
-    scan_msg.range_max = 5.0;
+    scan_msg.range_max = max_distance;//8.0;
+    // scan_msg.range_max = 5.0;
 
     scan_msg.intensities.resize(node_count);
     scan_msg.ranges.resize(node_count);
