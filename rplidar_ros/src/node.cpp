f438d2d1e0dd3bc06c8c3b733b46716a1bc1df8b
aditia08 <dit1a241@gmail.com>
2021-09-21 21:46:42 +0700

    a

--- rplidar_ros/src/node.cpp
+++ rplidar_ros/src/node.cpp
@@ -84,8 +84,8 @@ void publish_scan(ros::Publisher *pub,
     if (!reverse_data) {
         for (size_t i = 0; i < node_count; i++) {
             float read_value = (float) nodes[i].dist_mm_q2/4.0f/1000;
-            if (read_value == 0.0)
-                scan_msg.ranges[i] = std::numeric_limits<float>::infinity();
+            if (read_value <= 2.0)
+                scan_msg.ranges[i] = 2.0;//std::numeric_limits<float>::infinity();
             else
                 scan_msg.ranges[i] = read_value;
             scan_msg.intensities[i] = (float) (nodes[i].quality >> 2);
@@ -93,8 +93,8 @@ void publish_scan(ros::Publisher *pub,
     } else {
         for (size_t i = 0; i < node_count; i++) {
             float read_value = (float)nodes[i].dist_mm_q2/4.0f/1000;
-            if (read_value == 0.0)
-                scan_msg.ranges[node_count-1-i] = std::numeric_limits<float>::infinity();
+            if (read_value <= 2.0)
+                scan_msg.ranges[node_count-1-i] = 2.0;//std::numeric_limits<float>::infinity();
             else
                 scan_msg.ranges[node_count-1-i] = read_value;
             scan_msg.intensities[node_count-1-i] = (float) (nodes[i].quality >> 2);
