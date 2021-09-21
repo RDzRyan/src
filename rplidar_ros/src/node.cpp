8158abc164c2b4fe698ef0e09fdbefc8003c3617
aditia08 <dit1a241@gmail.com>
2021-09-21 15:40:15 +0700

    a

--- rplidar_ros/src/node.cpp
+++ rplidar_ros/src/node.cpp
@@ -267,14 +267,14 @@ int main(int argc, char * argv[]) {
     if(IS_OK(op_result))
     {
         //default frequent is 10 hz (by motor pwm value),  current_scan_mode.us_per_sample is the number of scan point per us
-        
-        angle_compensate_multiple = (int)(1000*1000/current_scan_mode.us_per_sample/10.0/360.0);
-        if(angle_compensate_multiple < 1) 
-          angle_compensate_multiple = 1;
+        float tesa=current_scan_mode.us_per_sample*2;
+        angle_compensate_multiple = (int)(1000*1000/tesa/10.0/360.0);
+        // if(angle_compensate_multiple < 1) 
+        //   angle_compensate_multiple = 1;
         max_distance = current_scan_mode.max_distance;
         ROS_INFO("current scan mode: %s, max_distance: %.1f m, Point number: %.1fK , angle_compensate: %d",  current_scan_mode.scan_mode,
                  current_scan_mode.max_distance, (1000/current_scan_mode.us_per_sample), angle_compensate_multiple);
-        ROS_INFO("%f",current_scan_mode.us_per_sample);
+        ROS_INFO("%f",tesa);
     }
     else
     {
