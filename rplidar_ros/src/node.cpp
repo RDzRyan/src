d1599c43b7af457c01de2fb041a7d88966166351
aditia08 <dit1a241@gmail.com>
2021-09-21 16:43:09 +0700

    a

--- rplidar_ros/src/node.cpp
+++ rplidar_ros/src/node.cpp
@@ -267,7 +267,7 @@ int main(int argc, char * argv[]) {
     if(IS_OK(op_result))
     {
         //default frequent is 10 hz (by motor pwm value),  current_scan_mode.us_per_sample is the number of scan point per us
-        current_scan_mode.us_per_sample=756.0;  //126.0
+        current_scan_mode.us_per_sample=2268.0;  //126.0 *18
         angle_compensate_multiple = (1000*1000/current_scan_mode.us_per_sample/10.0/360.0);
         // if(angle_compensate_multiple < 1) 
         //   angle_compensate_multiple = 1;
