46b1a4173aba1cf75532085d5d1a7c3b45e213ba
aditia08 <dit1a241@gmail.com>
2021-09-22 00:50:00 +0700

    a

--- navstack_pub/src/move3.cpp
+++ navstack_pub/src/move3.cpp
@@ -52,7 +52,7 @@ std::map<char, std::vector<float>> moveBindings{
     {'C', {-1, 1, 0, 0}}};
 
 //step
-char a_gerak[]  ={'w','a','a','s','D','w','a','w','A','w','Q','C','d','w','s'};
+char a_gerak[]  ={'w','a','q','s','D','w','a','w','A','w','Q','C','d','w','s'};
 
 std::map<int, std::vector<float>> step{
   // {1, {0,0,0,0,0,0,0,0}},
