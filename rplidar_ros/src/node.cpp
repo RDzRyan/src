20f712fb8e392110862158c7d5c219ad06c9eed8
aditia08 <dit1a241@gmail.com>
2021-09-21 10:41:48 +0700

    a

--- rplidar_ros/src/node.cpp
+++ rplidar_ros/src/node.cpp
@@ -76,8 +76,8 @@ void publish_scan(ros::Publisher *pub,
     scan_msg.scan_time = scan_time;
     scan_msg.time_increment = scan_time / (double)(node_count-1);
     scan_msg.range_min = 0.15;
-    // scan_msg.range_max = max_distance;//8.0;
-    scan_msg.range_max = 0.5;
+    scan_msg.range_max = max_distance;//8.0;
+    // scan_msg.range_max = 0.5;
 
     scan_msg.intensities.resize(node_count);
     scan_msg.ranges.resize(node_count);
@@ -86,7 +86,7 @@ void publish_scan(ros::Publisher *pub,
         for (size_t i = 0; i < node_count; i++) {
             float read_value = (float) nodes[i].dist_mm_q2/4.0f/1000;
             if (read_value == 0.0)
-                scan_msg.ranges[i] = std::numeric_limits<float>::infinity();
+                scan_msg.ranges[i] = 5.0;//std::numeric_limits<float>::infinity();
                 // scan_msg.ranges[i] = 5.0;
             else
                 scan_msg.ranges[i] = read_value;
@@ -96,7 +96,7 @@ void publish_scan(ros::Publisher *pub,
         for (size_t i = 0; i < node_count; i++) {
             float read_value = (float)nodes[i].dist_mm_q2/4.0f/1000;
             if (read_value == 0.0)
-                scan_msg.ranges[node_count-1-i] = std::numeric_limits<float>::infinity();
+                scan_msg.ranges[node_count-1-i] = 5.0;//std::numeric_limits<float>::infinity();
                 // scan_msg.ranges[i] = 5.0;
             else
                 scan_msg.ranges[node_count-1-i] = read_value;
