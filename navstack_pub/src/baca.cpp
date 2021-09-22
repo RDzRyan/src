ee7b886ce2910b14cadfcb0df81ba2c90c537883
aditia08 <dit1a241@gmail.com>
2021-09-22 23:11:40 +0700

    a

--- navstack_pub/src/baca.cpp
+++ navstack_pub/src/baca.cpp
@@ -3,7 +3,7 @@
 // #include "stdlib.h"
 
 
-int Index1,Index2,Index3,Index4,Index5,Index6;
+float Index1,Index2,Index3,Index4,Index5,Index6;
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
