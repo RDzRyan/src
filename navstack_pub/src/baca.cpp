77d3cd90683359d36da9177b99fa0697261058e4
aditia08 <dit1a241@gmail.com>
2021-09-22 23:11:24 +0700

    a

--- navstack_pub/src/baca.cpp
+++ navstack_pub/src/baca.cpp
@@ -7,15 +7,13 @@ int Index1,Index2,Index3,Index4,Index5,Index6;
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  string myString=msg->data.c_str();
-  if (myString.length()>0)
+  msg->data.c_str();
+  if (msg->data.c_str().length()>0)
 {
-Index1 = myString.indexOf('|');
-Index2 = myString.indexOf('|', Index1+1);
-Index3 = myString.indexOf('|', Index2+1);
-Index4 = myString.indexOf('|', Index3+1);
-Index5 = myString.indexOf('|', Index4+1);
-Index6 = myString.indexOf('|', Index5+1);
+Index1 = msg->data.c_str().indexOf('/');
+Index2 = msg->data.c_str().indexOf('/', Index1+1);
+Index3 = msg->data.c_str().indexOf('/', Index2+1);
+
 }
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
