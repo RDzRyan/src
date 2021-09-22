78ecf17c29f9e4ce54fea2d3d2abaa0e8b5dc336
aditia08 <dit1a241@gmail.com>
2021-09-22 23:40:13 +0700

    a

--- navstack_pub/src/baca.cpp
+++ navstack_pub/src/baca.cpp
@@ -1,18 +1,9 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-// #include "stdlib.h"
-
-
-std_msgs::String Index1,Index2,Index3,Index4,Index5,Index6;
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  // msg->data.c_str();
-  std::getline(msg->data.c_str(), Index1.data, '|');
-  std::getline(msg->data.c_str(), Index2.data, '|');
-  std::getline(msg->data.c_str(), Index3.data, '|');
   ROS_INFO("I heard: [%s]", msg->data.c_str());
-   ROS_INFO("[%s]",Index1.data);
 }
  
 int main(int argc, char **argv)
@@ -24,3 +15,4 @@ int main(int argc, char **argv)
  
   return 0;
 }
+
