c1fabbc3f223ea70040c8470f3f58f2ef46fc899
aditia08 <dit1a241@gmail.com>
2021-09-23 00:36:42 +0700

    a

--- navstack_pub/src/baca2.cpp
+++ navstack_pub/src/baca2.cpp
@@ -268,9 +268,9 @@ int main(int argc, char **argv)
     //baca setpoin
     ROS_INFO("-------------------------");
     ROS_INFO("%f, %f, %f, %f, %f",xaa[0],xaa[1],xaa[2],xaa[3],xaa[4]);
-    // for(int i = 0; i < 9; i++) {
-    //   ROS_INFO(": [%f]", laser[i]);
-    // }
+    for(int i = 0; i < 9; i++) {
+      ROS_INFO(": [%f]", laser[i]);
+    }
     
     //eksekusi
       // kontrol(a_gerak[flag1],flag1);
