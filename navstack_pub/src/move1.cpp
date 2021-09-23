60a6d4c81dbf77c2e86819e21a6c8f7cb8de1cef
aditia08 <dit1a241@gmail.com>
2021-09-23 20:59:38 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -113,7 +113,7 @@ std::map<int, std::vector<float>> step{
   
   {12, {-0.363460,-4,-4,0,0,0,0,0,0.5,0.5}}, // Mundur setelah menyimpan korban 
   {13, {-4,-4,2.260794,0,0,0,0,0,0.5,0.5}}, // Rotate Kiri __menuju Ruang 2 (90)
-  {14, {0.393443,-4,-4,0,0,0,0,0,0.5,0.5}}, // Maju __menuju Ruang 2
+  {14, {0.396,-4,-4,0,0,0,0,0,0.5,0.5}}, // Maju __menuju Ruang 2
   {15, {-4,-4,-2.7645,0,0,0,0,0,0.5,0.5}}, // Rotate Kanan Menuju __menuju Ruang 2 (90)
   {16, {0.821873,-4,-4,0,0,0,0,0,0.5,0.5}}, // maju dari lorong Ruang 1 __menuju Ruang 2
   {17, {-4,-4,2.468280,0,0,0,0,0,0.5,0.5}}, // rotate kiri __menuju Ruang 2 (90)
