2f5bce5927722d27bc68db429e40e806ee701a7f
aditia08 <dit1a241@gmail.com>
2021-09-23 13:15:34 +0700

    a

--- navstack_pub/src/move1.cpp
+++ navstack_pub/src/move1.cpp
@@ -134,7 +134,7 @@ std::map<int, std::vector<float>> step{
   {16, {0.821873,-4,-4,0,0,0,0,0,0.5,0.5}}, // maju dari lorong Ruang 1 __menuju Ruang 2
   {17, {-4,-4,2.468280,0,0,0,0,0,0.5,0.5}}, // rotate kiri __menuju Ruang 2 (90)
   {18, {0.831475,-4,-4,0,0,0,0,0,0.5,0.5}}, // maju dari lorong menuju ke Ruang 2 __menuju Ruang 2
-  {19, {-4,-4,2.260794,,0,0,0,0,0,0.5,0.5}}, // Rotate kiri menuju Ruang 2 (90)
+  {19, {-4,-4,2.260794,0,0,0,0,0,0.5,0.5}}, // Rotate kiri menuju Ruang 2 (90)
   {20, {0.294402,-4,-4,0,0,0,0,0,0.5,0.5}}, // maju menuju titik pemadaman  
 
 // posisi pemadaman dengan mengandalkan kondisi IF
@@ -147,7 +147,7 @@ std::map<int, std::vector<float>> step{
   {24, {-0.382365,-4,-4,0,0,0,0,0,0.5,0.5}}, // mundur dari safety zone __menuju home
   {25, {-4,0.483040,-4,0,0,0,0,0,0.5,0.5}}, // crab kiri __menuju home
   {26, {1.717780,-4,-4,0,0,0,0,0,0.5,0.5}}, // maju menuju home __menuju home
-  {27, {-4,-0.385724,-4,0,0,0,0,0,0.5,0.5}}, // crab kiri __menuju home
+  {27, {-4,-0.385724,-4,0,0,0,0,0,0.5,0.5}} // crab kiri __menuju home
 
 
   // {10, {0,0,0,0,0,0,0,0,0,0}}
