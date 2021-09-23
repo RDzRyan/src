5daddbf3d72c772e73b174ccaa52ec2d44b2bd2a
aditia08 <dit1a241@gmail.com>
2021-09-24 06:31:24 +0700

    aa

--- sensors/scripts/listener2.py
+++ sensors/scripts/listener2.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import String
 import time
 from std_msgs.msg import UInt16
-
+import UInt16 as a
 #urusan sama GPIO
 kondisi = ""
 
@@ -29,8 +29,8 @@ def callback(arduinoData):
         ta7 = int(int(parsed[7] + '0')/10)
         ta8 = int(int(parsed[8] + '0')/10)
         uvtron = int(int(parsed[9] + '0')/10)
-
-        pub_ir.publish(ir)
+        a.data=ir
+        pub_ir.publish(a)
 
 def listener():
     head = rospy.Publisher('head_scalar', String, queue_size=10)
